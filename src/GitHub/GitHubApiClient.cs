975258ba8419fff30d32d6fb537610f2b7b98a78
Tomasz Dłuski <mr.toumash@gmail.com>
2023-02-03 13:21:23 +0100

    throw if error

--- src/GitHub/GitHubApiClient.cs
+++ src/GitHub/GitHubApiClient.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Authenticators;
@@ -22,6 +24,13 @@ public class GitHubApiClient
     {
         var request = new RestRequest($"/repos/{repoUrl}/actions/artifacts?per_page=10&page=1");
         var response = await _client.ExecuteAsync<GetArtifactResponse>(request);
+        response.ThrowIfError();
+        
+        if (response.Data == null)
+        {
+            throw new Exception($"Invalid token exception. Response is null. Response Code = {response.StatusCode}");
+        }
+
         return response.Data.artifacts.First().archive_download_url;
     }
 
